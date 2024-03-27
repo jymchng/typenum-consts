f114417b7343fd0664a4ade9d7b1a0864e5e901d
root <root@vmi1175516.contaboserver.net>
2024-03-27 16:58:36 +0100

    changed folder structure to workspace to vendor rsc effectively

--- tests/trybuild.rs
+++
@@ -1,8 +0,0 @@
-use trybuild::TestCases;
-
-#[test]
-fn ui() {
-    let t = TestCases::new();
-    t.compile_fail("trybuild_tests/compile_fails/*.rs");
-    t.pass("trybuild_tests/compile_passes/*.rs");
-}
